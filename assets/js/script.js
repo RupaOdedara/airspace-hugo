2e3c1a11a0e4d401b20b54565d648110e7ca6d4b
rupaodedara <rupa.odedara.fatah@gmail.com>
2020-06-02 11:39:25 +0530

    add script.

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,4 +95,20 @@ $(document).ready(function () {
 		counter();
 	});
 
+	var liText = '', liList = $('.unique li'), listForRemove = [];
+
+  $(liList).each(function(){
+     var text = $(this).text();
+     if(liText.indexOf('|'+ text + '|') == -1)
+     {
+      liText += '|'+ text + '|';
+     }
+     else
+     {
+      listForRemove.push($(this));
+     }
+
+  });
+
+  $(listForRemove).each(function () { $(this).remove(); });
 });
